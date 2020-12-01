d495582a718ad988d6a1ea2e9c0b40b2b10d3635
Himanshu Sharma <69843278+himamshudp@users.noreply.github.com>
2020-12-01 15:17:41 +0530

    Update bigbascket_firebase.py

--- bigbascket_firebase.py
+++ bigbascket_firebase.py
@@ -34,12 +34,12 @@ def Bigbascket_scraper():
     chrome_options.add_argument("--no-sandbox")
     
     #for cloud
-    #chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    #driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     
     #forlocal machine
-    chromedriver = 'E://chromedriver.exe'
-    driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=chrome_options)
+    #chromedriver = 'E://chromedriver.exe'
+    #driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=chrome_options)
     url_scrap()
     url_selected = pd.read_csv(r'bb_selected_link.csv')
     product_url =  url_selected['link'].tolist()
